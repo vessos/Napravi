b8d9d13e35d71bab7d2eca9c6963e7bf212ba4d4
V.Manikatov <vinsent_vsm@abv.bg>
2017-01-16 23:19:17 +0200

    create login and logout ,create error box

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -11,10 +11,29 @@ function startApp() {
     $("#linkContacts").click(contactView);
     $("#linkLogout").click(logoutView);
 
+    //Bind from submit button
+
+    $("#buttonLoginUser").click(loginUser);
+
+    const kinveyBaseUrl = "https://baas.kinvey.com/";
+    const kinveyAppId = "kid_H1LDM2cIl";
+    const kinveyAppSecret = "a48d17d841294b73a1247b4e2ea597c9";
+    const kinveyAutHeaders = {"Authorization": "Basic "+ btoa(kinveyAppId + ":" + kinveyAppSecret)};
+
+    $(document).on({
+        ajaxStart: function(){
+            $("#loadingBox").show();
+        },
+        ajaxStop: function(){
+            $("#loadingBox").hide();
+        }
+    });
+
     function showMenu (){
         if(sessionStorage.getItem("authToken")){
             $("#linkHome").show();
             $("#linkLogin").hide();
+            $("#linkEdit").show();
             $("#linkGallery").show();
             $("#linkListPrice").show();
             $("#linkContacts").show();
@@ -22,6 +41,7 @@ function startApp() {
         }else{
             $("#linkHome").show();
             $("#linkLogin").show();
+            $("#linkEdit").hide();
             $("#linkGallery").show();
             $("#linkListPrice").show();
             $("#linkContacts").show();
@@ -55,8 +75,79 @@ function startApp() {
         showView("viewContact")
     }
 
+    function loginUser(){
+
+        let body =JSON.stringify({
+            username: $("#formLogin input[name=username]").val(),
+            password: $("#formLogin input[name=passwd]").val()
+        });
+        $.ajax({
+            method:"POST",
+            url:kinveyBaseUrl+"user/"+kinveyAppId+"/login",
+            headers:kinveyAutHeaders,
+            contentType:"application/json",
+            data:body,
+            success:loginSuccess,
+            error:getError
+        });
+
+        function loginSuccess(data){
+            sessionStorage.setItem("username",data.username);
+            sessionStorage.setItem("authToken",data._kmd.authtoken);
+            sessionStorage.setItem("userId",data._id);
+            $("#loggedInUser").text("Welcome "+ data.username +"!");
+            $("#formLogin input[name=username]").val("");
+            $("#formLogin input[name=passwd]").val("");
+
+            $("#loggedInUser").show();
+            showMenu();
+            $("#viewLogin").hide();
+            showInfo("success");
+
+        }
+    }
+
     function logoutView(){
-        alert("logout")
+        $.ajax({
+            method:"POST",
+            url:kinveyBaseUrl+"user/"+kinveyAppId+"/_logout",
+            headers:{"Authorization":"Kinvey "+sessionStorage.getItem("authToken")},
+            body:JSON.stringify(""),
+            success:successLogout,
+            error:getError
+        });
+
+        function successLogout(index){
+            sessionStorage.clear();
+            showInfo("success logout")
+            showView('viewAppHome');
+            $("#loggedInUser").empty();
+            showMenu();
+        }
+    }
+
+    function getError(response){
+        let errorMsg = JSON.stringify(response);
+        if (response.readyState === 0)
+            errorMsg = "Cannot connect due to network error.";
+        if (response.responseJSON &&
+            response.responseJSON.description)
+            errorMsg = response.responseJSON.description;
+        Error(errorMsg);
+    }
+
+    function Error(errorMsg){
+        console.dir(errorMsg)
+        $('#errorBox').text("Error: " + errorMsg);
+        $('#errorBox').show();
+    }
+
+    function showInfo(message){
+        $("#infoBox").text(message);
+        $("#infoBox").show();
+        setTimeout(function(){
+            $("#infoBox").fadeOut();
+        },2000)
     }
 
 
